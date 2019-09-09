89a560a7bef5145cff9d4f9c87cc9526e33ded74
Jessica Evans <jme@mail.usf.edu>
2019-09-08 20:57:01 -0400

    tried to add employee

--- src/Pages.jsx/AddEmployee.jsx
+++ src/Pages.jsx/AddEmployee.jsx
@@ -1,31 +1,61 @@
-import React, { useState } from "react"
-import DatePicker from "react-datepicker"
-import "react-datepicker/dist/react-datepicker.css"
+// import React, { useState, useEffect } from "react"
+// import DatePicker from "react-datepicker"
+// import axios from "axios"
+// import "react-datepicker/dist/react-datepicker.css"
 
-const AddEmployee = () => {
-  const [startDate, setStartDate] = useState("")
-  return (
-    <>
-      <form className="add-employee-form" action="">
-        <div>
-          <label htmlFor="name">First Name:</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label htmlFor="last-name">Last Name:</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label htmlFor="birthday">Date of Birth:</label>
-          <DatePicker
-            className="date-picker"
-            selected={startDate}
-            onChange={date => setStartDate(date)}
-          />
-        </div>
-      </form>
-    </>
-  )
-}
+// const AddEmployee = () => {
+//   const [startDate, setStartDate] = useState("")
+//   const postData = async () => {
+//     const resp = await axios.post(
+//       "https://sdg-staff-directory-app.herokuapp.com/api/Business%20Company/Employees",
+//       { firstName,
+//         lastName,
+//         birthday,
+//         hiredDate	,
+//         isFullTime	,
+//         profileImage,
+//         jobTitle	,
+//         jobDescription,
+//         phoneNumber	,
+//         interestingFact,
+//         address	,
+//         city,
+//         zip	,
+//         state	,
+//         salary	,
+//         gender	,
+//         email,
+//         emergencyContactPerson	,
+//         emergencyContactPhone	,
+//         emergencyContactAddress,
+//         ptoHours
+//     )
+//     )
 
-export default AddEmployee
+//   }
+//   return (
+//     <>
+//       <h1 className="directory-title">Add an Employee</h1>
+//       <form className="add-employee-form" action="">
+//         <div>
+//           <label htmlFor="name">First Name:</label>
+//           <input type="text" />
+//         </div>
+//         <div>
+//           <label htmlFor="last-name">Last Name:</label>
+//           <input type="text" />
+//         </div>
+//         <div>
+//           <label htmlFor="birthday">Date of Birth:</label>
+//           <DatePicker
+//             className="date-picker"
+//             selected={startDate}
+//             onChange={date => setStartDate(date)}
+//           />
+//         </div>
+//       </form>
+//     </>
+//   )
+// }
+
+// export default AddEmployee
